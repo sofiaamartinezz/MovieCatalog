4df7d62d7d538ad77b41c01d88355e4ab15e79d4
sofiaamartinezz <soff.m.p.03@gmail.com>
2024-12-17 19:40:44 +0100

    improvement of .gitignore

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -8,7 +8,7 @@ public class Movie
     public string? Title { get; set; }
     public string? Genre { get; set; }
     public string? Director { get; set; }
-    public string? Description { get; set; }
+    public string? Rating { get; set; }
 
     // Propiedad de navegación para la relación con UserMovie
     public ICollection<UserMovie>? UserMovies { get; set; }
